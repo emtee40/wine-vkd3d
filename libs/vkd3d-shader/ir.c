67c6ea3ef318b5f67dbd44730df3ccfc306e731b
Giovanni Mascellani <gmascellani@codeweavers.com>
2024-11-21 15:22:27 +0100

    vkd3d-shader/ir: Validate index count for PRIMID registers.

--- libs/vkd3d-shader/ir.c
+++ libs/vkd3d-shader/ir.c
@@ -7185,6 +7185,10 @@ static void vsir_validate_register(struct validation_context *ctx,
             vsir_validate_register_without_indices(ctx, reg);
             break;
 
+        case VKD3DSPR_PRIMID:
+            vsir_validate_register_without_indices(ctx, reg);
+            break;
+
         case VKD3DSPR_NULL:
             vsir_validate_register_without_indices(ctx, reg);
             break;
